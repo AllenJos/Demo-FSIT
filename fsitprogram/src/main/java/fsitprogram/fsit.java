6a627857344db29b65ca6e046bdb9208541a0eaa
AllenJos <allenjos1996@gmail.com>
2020-08-28 23:29:10 +0530

    Demo

---
+++ fsitprogram/src/main/java/fsitprogram/fsit.java
@@ -0,0 +1,10 @@
+package fsitprogram;
+
+public class fsit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My first maven project");
+	}
+
+}
